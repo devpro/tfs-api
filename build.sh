8d5dd459f842b68ff3961eb2c5b8407f933ac946
Bertrand THOMAS <bertrand@devpro.fr>
2016-10-22 21:51:36 +0200

    Converted end of line for travis files

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 artifactsFolder="./artifacts"
 
-echo 'Looking if '$artifactsFolder' folder exists'
+echo "Looking if $artifactsFolder folder exists"
 if [ -d $artifactsFolder ]; then
-    echo 'Deleting '$artifactsFolder' folder'
+    echo "Deleting $artifactsFolder folder"
     rm -R $artifactsFolder
 fi
 
@@ -16,7 +16,7 @@ dotnet build ./src/Devpro.TfsApi -c Release -f net452 --no-incremental
 
 revision=${TRAVIS_JOB_ID:=1}
 revision=$(printf "beta%04d" $revision)
-echo 'Revision is "'$revision'"'
+echo "Revision is $revision"
 
 echo 'Launching dotnet pack'
 dotnet pack ./src/Devpro.TfsApi.Dto -c Release -o ./artifacts --version-suffix=$revision
