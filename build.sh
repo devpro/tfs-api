0375677ef6eff20c2de1761a58317872453d678b
Bertrand THOMAS <bertrand@devpro.fr>
2016-10-22 19:00:51 +0200

    Added display messages in build sh to investigate travis build error

--- build.sh
+++ build.sh
@@ -5,16 +5,21 @@ set -e
 
 artifactsFolder="./artifacts"
 
+echo 'Looking if '$artifactsFolder' folder exists'
 if [ -d $artifactsFolder ]; then
-  rm -R $artifactsFolder
+    echo 'Deleting '$artifactsFolder' folder'
+    rm -R $artifactsFolder
 fi
 
+echo 'Launching dotnet restore'
 dotnet restore
 
+echo 'Launching dotnet build'
 dotnet build ./src/Devpro.TfsApi -c Release -f net452 --no-incremental
 
 revision=${TRAVIS_JOB_ID:=1}
 revision=$(printf "beta%04d" $revision)
 echo 'Revision is "'$revision'"'
 
+echo 'Launching dotnet pack'
 dotnet pack ./src/Devpro.TfsApi.Dto -c Release -o ./artifacts --version-suffix=$revision
