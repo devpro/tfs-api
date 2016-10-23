290e136632b9cf044cd3848efab98919e9579891
Bertrand THOMAS <bertrand@devpro.fr>
2016-10-23 13:01:44 +0200

    Updated build.sh to launch unit tests

--- build.sh
+++ build.sh
@@ -13,6 +13,12 @@ dotnet restore
 
 echo 'Launching dotnet build'
 dotnet build ./src/Devpro.TfsApi -c Release -f net452 --no-incremental
+dotnet build ./test/Devpro.TfsApi.Lib.Tests -c Release -f net452
+
+echo 'Launching test with mono'
+mono \
+./test/Devpro.TfsApi.Lib.Tests/bin/Release/net452/*/dotnet-test-xunit.exe \
+./test/Devpro.TfsApi.Lib.Tests/bin/Release/net452/*/Devpro.TfsApi.Lib.Tests.dll
 
 revision=${TRAVIS_JOB_ID:=1}
 revision=$(printf "beta%04d" $revision)
